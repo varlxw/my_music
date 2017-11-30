0f60c3177cc6633f67fa41c7085bc490e468b10c
luxw <luxiaow108@163.com>
2017-11-30 23:35:57 +0800

    初始化项目-项目目录-common

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    hello world
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    name: 'app'
+  }
+</script>
+
+<style lang="stylus" scoped>
+  @import "common/stylus/variable.styl"
+  #app
+    color: $color-theme
+</style>
